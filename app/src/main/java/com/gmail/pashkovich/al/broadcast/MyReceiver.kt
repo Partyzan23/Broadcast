28e3ce0f83a2803a9849d5298daa18700e9e6e5b
Alex Pashkovich <al.pashkovich@gmail.com>
2023-05-29 14:32:44 +0200

    Create custom receiver

--- app/src/main/java/com/gmail/pashkovich/al/broadcast/MyReceiver.kt
+++ app/src/main/java/com/gmail/pashkovich/al/broadcast/MyReceiver.kt
@@ -7,7 +7,15 @@ import android.widget.Toast
 
 class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        when (intent?.action){
+        when (intent?.action) {
+            ACTION_CLICKED -> {
+                val count = intent.getIntExtra(EXTRA_COUNT_CLICK, 0)
+                Toast.makeText(
+                    context,
+                    "Button clicked $count",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
                 val turnedOn = intent.getBooleanExtra("state", false)
                 Toast.makeText(
@@ -20,5 +28,10 @@ class MyReceiver : BroadcastReceiver() {
                 Toast.makeText(context, "Low battery", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    companion object {
+        const val ACTION_CLICKED = "clicked"
+        const val EXTRA_COUNT_CLICK = "count"
     }
 }
